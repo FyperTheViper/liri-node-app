f9935bafd2bc10cf07157c9dc0a981f377580c4c
FyperTheViper <b.w.byars@gmail.com>
2018-11-28 16:11:44 -0800

    cleaned and added file append

--- liri.js
+++ liri.js
@@ -5,13 +5,14 @@ let request = require('request');
 let moment = require('moment');
 let fs = require("fs");
 let chalk = require('chalk');
-let log = console.log
+let log = console.log;
 let spotify = new Spotify(keys.spotify);
-let spacing = "";
+let spacing = " ";
 
 let caseLine = process.argv[2];
 let userLine = process.argv[3];
 
+//*******WELCOME TO LIRI **********//
 switch (caseLine) {
     case "concert-this":
         concertThis();
@@ -27,63 +28,74 @@ switch (caseLine) {
         break;
 }
 
+//Greeting message
+log(chalk.grey("\nWelcome to LIRI! Type in a command below followed by a song, artist or movie:"), chalk.green("\nconcert-this  spotify-this-song  movie-this  do-what-it-says") + spacing);
+
+//Bands in Town
 function concertThis() {
     if (userLine == undefined) {
         log("Type in a musician or band to see their upcoming concerts.");
-    };
-    request("https://rest.bandsintown.com/artists/" + userLine + "/events?app_id=codingbootcamp", function (err, response, body) {
-
+    }
+    request("https://rest.bandsintown.com/artists/" + userLine + "/events?app_id=codingbootcamp", function (error, response, body) {
+        saveSearch();
         // A status code 200 means a successful request in HTTP
-        if (!err && response.statusCode === 200) {
+        if (!error && response.statusCode === 200  && userLine != undefined) {
             let jBody = JSON.parse(body);
             for (i = 0; i < jBody.length; i++) {
                 let showConcert =
-                    chalk.grey("\n****************************** BANDS IN TOWN RESULT *******************************\n") +
-                    "\nVenue: " + chalk.green.bold(jBody[i].venue.name) +
-                    "\nCity: " + chalk.green.bold(jBody[i].venue.city + ", " + jBody[i].venue.country) +
-                    "\nDate: "+ chalk.green.bold(moment(jBody[i].datetime).format("MM/DD/YY"));
+                    "\n****************************** BANDS IN TOWN RESULT *******************************\n" +
+                    "\nVenue: " + jBody[i].venue.name +
+                    "\nCity: " + jBody[i].venue.city + ", " + jBody[i].venue.country +
+                    "\nDate: "+ moment(jBody[i].datetime).format("MM/DD/YY");
                 log(showConcert);
+                logResults(showConcert);
             }
         } else {
-            log(err);
+            return log(error);
         }
     });
 }
 
+//Spotify track search query
 function spotifyThis() {
     if (userLine == undefined) {
-        userLine = "The Sign Ace of Base"
-        log("No user input found, so here's the best song in the world.");
+        userLine = "The Sign Ace of Base";
+        log(chalk.grey("\nNo user input found, so here's the best song in the world."));
     };
+    saveSearch();
     spotify.search({
         type: 'track',
         query: userLine,
-    }, function (err, data) {
-        if (!err && data) {
-            let narrow = data.tracks.items
+    }, function (error, data) {
+        if (!error && data) {
+            let narrow = data.tracks.items;
             let showSpotify =
-                chalk.grey("\n****************************** SPOTIFY RESULT *******************************\n") +
-                "\nArtist: " + chalk.green.bold(narrow[0].artists[0].name) +
-                "\nSong title: " + chalk.green.bold(narrow[0].name) +
-                "\nAlbum name: " + chalk.green.bold(narrow[0].album.name) +
+                "\n****************************** SPOTIFY RESULT *******************************\n" +
+                "\nArtist: " + narrow[0].artists[0].name +
+                "\nSong title: " + narrow[0].name +
+                "\nAlbum name: " + narrow[0].album.name +
                 "\nURL Preview: " + narrow[0].preview_url +
                 "\n" + spacing + "\n";
             log(showSpotify);
-        };
+            logResults(showSpotify);
+        } else {
+            return log(error);
+        }
     });
-};
+}
 
+//OMDB movie info dump
 function movieThis() {
     if (userLine == undefined) {
         userLine = "Mr. Nobody"
-        log("Here's an overrated movie, enjoy");
-    };
+        log(chalk.grey("\nHere's an overrated movie, enjoy"));
+    }
     request("http://www.omdbapi.com/?t=" + userLine + "&y=&plot=short&apikey=trilogy", function(error, response, data) {
-
+    saveSearch();
     if (!error && response.statusCode === 200 && userLine != undefined) {
         let jBody = JSON.parse(data);
         let showMovie =
-            chalk.grey("\n****************************** OMDB RESULT *******************************\n") +
+            "\n****************************** OMDB RESULT *******************************\n" +
             "\nTitle: " + jBody.Title +
             "\nYear Released: " + jBody.Year +
             "\nIMDB Rating: " + jBody.imdbRating +
@@ -94,10 +106,14 @@ function movieThis() {
             "\nActors: " + jBody.Actors +
             "\n" + spacing + "\n";
         log(showMovie);
+        logResults(showMovie);
+    } else {
+        return log(error);
     }
 });
-};
+}
 
+//Reads contents of random.txt, runs spotify function
 function doThis() {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
@@ -110,4 +126,15 @@ function doThis() {
             spotifyThis();
         }
     });
+}
+
+//Adds search terms and results to log.txt file
+function logResults(results) {
+    fs.appendFile("log.txt", results, function(error, logged) {
+        if (error) return log(error);
+    });
+}
+
+function saveSearch() {
+    logResults("\n\n____________________NEW  " + caseLine + "  SEARCH_____________________\nRESULTS FOR:\n" + userLine + "\n");
 }
